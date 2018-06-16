075e7b1de3e74dcd7943ddb15265fceda2516744
maurice@alu <jmulisa15@alustudent.com>
2018-06-16 13:58:12 +0400

    changed print message

--- src/Week5/JosephusProblem.java
+++ src/Week5/JosephusProblem.java
@@ -9,6 +9,16 @@ package Week5;
  *
  * @author Maurice
  */
+
+/*
+Given the total number of persons n and a number k which indicates that k-1 persons are skipped 
+and kth person is killed in circle.The task is to choose the place in the initial circle so that 
+you are the last one remaining and so survive.
+
+For example, if n = 5 and k = 2, then the safe position is 3. Firstly, the person at position 2 is killed,
+then person at position 4 is killed, then person at position 1 is killed. Finally, the person at position 5 is killed.
+So the person at position 3 survives.
+*/
 public class JosephusProblem {
    static int josephus(int n, int k)
 {
@@ -18,7 +28,9 @@ else
     /* The position returned by josephus(n - 1, k) 
     is adjusted because the recursive call 
     josephus(n - 1, k) considers the original 
-    position k%n + 1 as position 1 */
+    position k%n + 1 as position 1 
+    
+    */
     return (josephus(n - 1, k) + k-1) % n + 1;
 }
  
@@ -29,6 +41,6 @@ int n = 14;
 int k = 2;
 System.out.println("Number of People: " + n);
 System.out.println("People to skip: " + k);
-System.out.println("The chosen place is: " + josephus(n, k));
+System.out.println("The chosen(safe) place is: " + josephus(n, k));
 } 
 }
